548f823e2d32de5eeea4dff8d513673ebdf8f7b8
Martim Passos <martim.passos@alumni.usp.br>
2025-05-12 15:50:29 -0300

    feat: define rotas login, sobre, explore e contribua

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,27 @@
 export default [
-	{
-        path: '/',
-        name: 'welcome',
-        component: () => import('../views/Home.vue')
-	},
-	{
-        path: '/about',
-        name: 'about',
-        component: () => import('../views/About.vue')
-	},
-	{
-        path: '/login',
-        name: 'login',
-        component: () => import('../views/Auth/Login.vue')
-	}
-]
+  {
+    path: "/",
+    name: "explore",
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Auth/Login.vue"),
+  },
+  //   {
+  //     path: "/explore",
+  //     name: "explore",
+  //     component: () => import("../views/Explore.vue"),
+  //   },
+  {
+    path: "/contribua",
+    name: "contribua",
+    component: () => import("../views/Contribua.vue"),
+  },
+];
