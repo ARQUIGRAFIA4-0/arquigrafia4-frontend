d0ec62c8038b4262fdf84d80e55675858d330213
Davi <davi.a.teodoro@gmail.com>
2025-09-01 13:52:03 -0300

    Configura cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,31 @@
+/* eslint-disable no-undef */
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-essential", "prettier"],
-  overrides: [],
+  overrides: [
+    {
+      files: [
+        "cypress/**/*.js",
+        "src/**/*.cy.js",
+        "src/**/*.cy.jsx",
+        "src/**/*.cy.ts",
+        "src/**/*.cy.tsx",
+      ],
+      env: {
+        browser: true,
+        es2021: true,
+        mocha: true,
+      },
+      globals: {
+        cy: "readonly",
+        Cypress: "readonly",
+      },
+      rules: {},
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
