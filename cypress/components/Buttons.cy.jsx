3d17c79428d1fcaae5ca66991b0550435076ee09
Davi <davi.a.teodoro@gmail.com>
2025-09-09 20:13:25 -0300

    Adiciona testes para renderização de dropdowns no componente de botões, incluindo diferentes variantes e estilos, além de ajustes nos estilos SCSS para suportar a posição do caret à direita.

--- cypress/components/Buttons.cy.jsx
+++ cypress/components/Buttons.cy.jsx
@@ -164,4 +164,163 @@ describe("Buttons (Bootstrap styles)", () => {
       </div>
     ));
   });
+
+  it("renderiza dropdowns", () => {
+    cy.mount(() => (
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 280px)",
+          gap: "16px",
+          alignItems: "start",
+          justifyItems: "start",
+        }}
+      >
+        {/* Básico (fechado) */}
+        <div class="btn-group">
+          <button
+            type="button"
+            class="btn btn-primary dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Primary
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                Separated link
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Aberto (forçado com .show) com caret à esquerda (padrão - sem classe) */}
+        <div class="dropdown show">
+          <button
+            class="btn btn-primary btn-icon dropdown-toggle"
+            type="button"
+          >
+            <i class="bi bi-sticky" /> Dropdown
+          </button>
+          <ul
+            class="dropdown-menu show"
+            style={{ position: "static", transform: "none", display: "block" }}
+          >
+            <li>
+              <button class="dropdown-item active">Active</button>
+            </li>
+            <li>
+              <button class="dropdown-item">Item</button>
+            </li>
+            <li>
+              <button class="dropdown-item" disabled>
+                Disabled
+              </button>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <button class="dropdown-item">Separated link</button>
+            </li>
+          </ul>
+        </div>
+
+        {/* Split button com caret à direita */}
+        <div class="dropdown show">
+          <button
+            class="btn btn-secondary btn-icon dropdown-toggle caret-right"
+            type="button"
+          >
+            <i class="bi bi-sticky" /> Dropdown
+          </button>
+          <ul
+            class="dropdown-menu show"
+            style={{ position: "static", transform: "none", display: "block" }}
+          >
+            <li>
+              <button class="dropdown-item active">Active</button>
+            </li>
+            <li>
+              <button class="dropdown-item">Item</button>
+            </li>
+            <li>
+              <button class="dropdown-item" disabled>
+                Disabled
+              </button>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <button class="dropdown-item">Separated link</button>
+            </li>
+          </ul>
+        </div>
+
+        {/* Tamanho sm */}
+        <div class="dropdown">
+          <button class="btn btn-primary btn-sm dropdown-toggle" type="button">
+            Small
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <button class="dropdown-item">Item</button>
+            </li>
+            <li>
+              <button class="dropdown-item">Item</button>
+            </li>
+          </ul>
+        </div>
+
+        {/* Outline e aberto */}
+        <div class="dropdown show">
+          <button class="btn btn-outline-primary dropdown-toggle" type="button">
+            Outline
+          </button>
+          <ul
+            class="dropdown-menu show"
+            style={{ position: "static", transform: "none", display: "block" }}
+          >
+            <li>
+              <button class="dropdown-item">Item</button>
+            </li>
+            <li>
+              <button class="dropdown-item">Item</button>
+            </li>
+          </ul>
+        </div>
+
+        {/* Disabled */}
+        <div class="dropdown">
+          <button
+            class="btn btn-outline-secondary dropdown-toggle"
+            type="button"
+            disabled
+          >
+            Disabled
+          </button>
+        </div>
+      </div>
+    ));
+  });
 });
