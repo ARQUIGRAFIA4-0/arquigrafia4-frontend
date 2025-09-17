5192c31f83afd615a4e09e05fcbff84eb714d036
Yuri Tavares <yuriatp@gmail.com>
2025-09-17 19:23:42 -0300

    ProfileCard: Adiciona elementos HTML do cabeçalho.

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -17,6 +17,20 @@ onMounted(async () => {
 </script>
 
 <template>
+    <div class="profile-card">
+        <div class="profile-card__header">
+            <div class="profile-card__image"></div>
+            <div class="profile-card__title">
+                <h2>{{ user.name }}</h2>
+                <div class="profile-card__address">
+                    <i class="bi bi-geo-alt"></i>
+                    <p>{{ profileData?.data.address || "Não informado" }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
