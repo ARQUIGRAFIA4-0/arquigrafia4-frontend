c538e4b171cf58b847b965c676044bb26a7f735d
Yuri Tavares <yuriatp@gmail.com>
2025-09-17 13:51:16 -0300

    ProfileCard: Obtém dados do usuário na rota '/eu'.

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+import { useAuthStore } from "@/store/auth";
+import { ref, computed, onMounted } from "vue";
 
-<template></template>
+const route = useRoute();
+const store = useAuthStore();
+const user = computed(() => store.loggedUser);
+
+const profileData = ref(null);
+
+onMounted(async () => {
+    if (route.path === "/eu") {
+        profileData.value = await store.getProfileById(user.value.id);
+    }
+});
+</script>
+
+<template>
+</template>
 
 <style lang="scss" scoped></style>
