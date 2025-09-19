e3bdc1c13736df44a322a0620b059fef7621b9c8
Yuri Tavares <yuriatp@gmail.com>
2025-09-19 10:34:07 -0300

    feat(EditProfileNav): Adiciona estilo do componente.

--- src/components/EditProfileNav.vue
+++ src/components/EditProfileNav.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <ul class="profile-nav">
+  <ul class="edit-profile-nav">
     <li>Dados pessoais</li>
     <li>Dados profissionais</li>
     <li>Temas de interesse</li>
@@ -16,6 +16,49 @@ $breakpoint-md: 768px;
 @mixin md {
   @media (min-width: #{$breakpoint-md}) {
     @content;
+  }
+}
+
+.edit-profile-nav {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  margin-bottom: 32px;
+  padding: 0px 0 0 0;
+
+  li {
+    margin-right: 32px;
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 100%;
+    letter-spacing: 0%;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+
+    @include md {
+      font-size: 20px;
+      margin-right: 48px;
+    }
+  }
+
+  &--selected {
+    color: #AA4F28;
+    font-weight: 800;
+    font-size: 12px;
+    line-height: 150%;
+    letter-spacing: 0%;
+    padding-bottom: 8px;
+    border-bottom: 4px solid;
+    text-align: center;
+    vertical-align: middle;
+
+    @include md {
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 100%;
+    }
   }
 }
 </style>
