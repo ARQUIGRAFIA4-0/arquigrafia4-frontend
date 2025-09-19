baa4cce493b0779628739b7a18b397942cf627f2
Yuri Tavares <yuriatp@gmail.com>
2025-09-19 11:13:37 -0300

    feat(EditProfileInterests): Cria componente.

---
+++ src/components/EditProfileInterests.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+    <div>Temas de interesse</div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/variables";
+$breakpoint-md: 768px;
+
+@mixin md {
+  @media (min-width: #{$breakpoint-md}) {
+    @content;
+  }
+}
+</style>
