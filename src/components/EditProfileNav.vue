75c9faeea12275001ef4f1d045501c4bfec0dcfb
Yuri Tavares <yuriatp@gmail.com>
2025-09-19 10:30:43 -0300

    feat(EditProfileNav): Cria componente.

---
+++ src/components/EditProfileNav.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <ul class="profile-nav">
+    <li>Dados pessoais</li>
+    <li>Dados profissionais</li>
+    <li>Temas de interesse</li>
+    <li>Coletivos</li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/variables";
+$breakpoint-md: 768px;
+
+@mixin md {
+  @media (min-width: #{$breakpoint-md}) {
+    @content;
+  }
+}
+</style>
