a2c39dbf3598a0aa23994c2658a3e6fe5165e901
Yuri Tavares <yuriatp@gmail.com>
2025-09-19 11:56:10 -0300

    feat(EditProfilePersonal): Cria componente.

---
+++ src/components/EditProfilePersonal.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  profileData: {
+    type: Object,
+    default: null
+  }
+});
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+@import "@/scss/variables";
+$breakpoint-md: 768px;
+
+@mixin md {
+  @media (min-width: #{$breakpoint-md}) {
+    @content;
+  }
+}
+</style>
