d19ef84ceb5b5db0d2cdfc68f091a72f537f86a8
Yuri Tavares <yuriatp@gmail.com>
2025-09-19 08:43:33 -0300

    ProfileNav: Adiciona estilo do componente.

--- src/components/ProfileNav.vue
+++ src/components/ProfileNav.vue
@@ -19,4 +19,47 @@ $breakpoint-md: 768px;
     @content;
   }
 }
+
+.profile-nav {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  margin-bottom: 32px;
+  padding: 0px 0 0 0;
+
+  li {
+    margin-right: 32px;
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 100%;
+    letter-spacing: 0%;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+
+    @include md {
+      font-size: 20px;
+      margin-right: 48px;
+    }
+  }
+
+  &--selected {
+    color: #AA4F28;
+    font-weight: 800;
+    font-size: 12px;
+    line-height: 150%;
+    letter-spacing: 0%;
+    padding-bottom: 8px;
+    border-bottom: 4px solid;
+    text-align: center;
+    vertical-align: middle;
+
+    @include md {
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 100%;
+    }
+  }
+}
 </style>
