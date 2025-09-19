ec44b3b460ca467ae2592f3ba1dccc28adc9431d
Yuri Tavares <yuriatp@gmail.com>
2025-09-19 08:54:20 -0300

    feat(ProfileImageGrid): Adiciona estilo do componente.

--- src/components/ProfileImageGrid.vue
+++ src/components/ProfileImageGrid.vue
@@ -13,4 +13,23 @@ $breakpoint-md: 768px;
     @content;
   }
 }
+
+.item-card {
+  &__image {
+    border-radius: 8px 8px 0 0;
+    width: 100%;
+    height: 120px;
+    background-color: #9c9c9c;
+    cursor: pointer;
+  }
+
+  &__text {
+    border: 1px solid #E0E0E0;
+    border-top: 0;
+    padding: 8px 12px;
+    border-radius: 0 0 8px 8px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+  }
+}
 </style>
