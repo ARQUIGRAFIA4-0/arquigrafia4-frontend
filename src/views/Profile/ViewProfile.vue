8c43611118717e120159c6f4ac39f496b6eac463
Yuri Tavares <yuriatp@gmail.com>
2025-09-20 20:31:19 -0300

    refactor(ViewProfile): Obtém dados de profile e user, repassa para 'ProfileCard'.

--- src/views/Profile/ViewProfile.vue
+++ src/views/Profile/ViewProfile.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { useAuthStore } from "@/store/auth";
 import ProfileCard from "@/components/ProfileCard.vue";
 import ProfileNav from "@/components/ProfileNav.vue";
 import ProfileWorks from "@/components/ProfileWorks.vue";
@@ -8,13 +10,24 @@ import ProfileRoutes from "@/components/ProfileRoutes.vue";
 import ProfileReviews from "@/components/ProfileReviews.vue";
 import ProfileImages from "@/components/ProfileImages.vue";
 
+const route = useRoute();
+const store = useAuthStore();
+const user = computed(() => store.loggedUser);
+
+const profileData = ref(null);
 const selectedTab = ref("Imagens");
+
+onMounted(async () => {
+  if (["/eu", "/eu/"].includes(route.path)) {
+    profileData.value = await store.getProfileById(user.value.id);
+  }
+});
 </script>
 
 <template>
   <div class="profile-container">
     <div class="col-12 col-md-4">
-      <ProfileCard />
+      <ProfileCard :profileData="profileData" :user="user" />
     </div>
     <div class="d-none d-md-block col-md-1"></div>
     <div class="col-12 col-md-7">
