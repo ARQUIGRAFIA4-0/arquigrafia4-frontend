7153c64dc7f285b135cb3140a44c81fa98795e44
Yuri Tavares <yuriatp@gmail.com>
2025-10-28 20:34:07 -0300

    feat(profiles): Adiciona função 'getProfileById'.

--- src/store/profiles.js
+++ src/store/profiles.js
@@ -16,7 +16,23 @@ export const useProfilesStore = defineStore("profiles", () => {
     }
   }
 
+  async function getProfileById(authHeader, userId) {
+    try {
+      const response = await axios.get(`/api/profiles/by-user-id/${userId}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": authHeader,
+        },
+      });
+      return response.data;
+    }
+    catch (error) {
+      throw Error("Não foi possível buscar o perfil.");
+    }
+  }
+
   return {
-    getPublicProfileById
+    getPublicProfileById,
+    getProfileById
   };
 });
