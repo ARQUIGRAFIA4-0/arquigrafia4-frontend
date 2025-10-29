26e376adf9dc29853ae275b0fdecae69f084095f
Yuri Tavares <yuriatp@gmail.com>
2025-10-29 19:07:28 -0300

    feat(ViewPrivateProfile): Cria nova view.

---
+++ src/views/Profile/ViewPrivateProfile.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, computed } from "vue";
+import { useAuthStore } from "../../store/auth";
+import { useProfilesStore } from "../../store/profiles";
+
+const authStore = useAuthStore();
+const profilesStore = useProfilesStore();
+
+const currentUserData = ref(authStore.loggedUser);
+const currentUserAuthHeader = computed(() => authStore.authHeader);
+
+</script>
+<template></template>
