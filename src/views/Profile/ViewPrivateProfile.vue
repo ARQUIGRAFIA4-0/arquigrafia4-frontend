e133ef8b5978d86b4902c72ca0e507104b69e9b4
Yuri Tavares <yuriatp@gmail.com>
2025-10-29 19:08:57 -0300

    feat(ViewPrivateProfile): Obtém dados públicos e privados do perfil.

--- src/views/Profile/ViewPrivateProfile.vue
+++ src/views/Profile/ViewPrivateProfile.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { useAuthStore } from "../../store/auth";
 import { useProfilesStore } from "../../store/profiles";
 
@@ -8,6 +8,14 @@ const profilesStore = useProfilesStore();
 
 const currentUserData = ref(authStore.loggedUser);
 const currentUserAuthHeader = computed(() => authStore.authHeader);
+
+const publicProfileData = ref(null);
+const privateProfileData = ref(null);
+
+onMounted(async () => {
+    publicProfileData.value = await profilesStore.getPublicProfileById(currentUserData.value.id);
+    privateProfileData.value = await profilesStore.getProfileById(currentUserAuthHeader.value, currentUserData.value.id);
+});
 
 </script>
 <template></template>
