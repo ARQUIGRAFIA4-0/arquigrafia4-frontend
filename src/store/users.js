d1e87fa85e8fb9d3dcb205b9c5cfca125aef14a2
Martim Passos <martim.passos@alumni.usp.br>
2026-04-15 12:13:01 -0300

    fix: ajusta formato de dados de usuário e perfil

--- src/store/users.js
+++ src/store/users.js
@@ -9,7 +9,7 @@ export const useUsersStore = defineStore("users", () => {
           "Content-Type": "application/json"
         },
       });
-      return response.data.user;
+      return response.data.data;
     } catch (error) {
       throw new Error("Não foi possível buscar o usuário.");
     }
